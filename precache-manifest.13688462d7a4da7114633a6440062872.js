af5e034d41de08cb29175f96e1799d020e0bc0c9
daoduytan <ddtan2212@gmail.com>
2019-09-03 08:51:08 +0700

    Updates

---
+++ precache-manifest.13688462d7a4da7114633a6440062872.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07958b976cab6b039002ee1bb61e4734",
+    "url": "/dict/index.html"
+  },
+  {
+    "revision": "10ab96acaf4088790efa",
+    "url": "/dict/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dc57976f5690950974d8",
+    "url": "/dict/static/js/10.35bad397.chunk.js"
+  },
+  {
+    "revision": "680dd4eafd3cea3cef60",
+    "url": "/dict/static/js/11.5f686860.chunk.js"
+  },
+  {
+    "revision": "10ab96acaf4088790efa",
+    "url": "/dict/static/js/2.62d7d431.chunk.js"
+  },
+  {
+    "revision": "e8ad53c3fd9820ef3ddd",
+    "url": "/dict/static/js/3.c9f4324c.chunk.js"
+  },
+  {
+    "revision": "e86dfca8ff9873b782dd",
+    "url": "/dict/static/js/4.6dfb5cb3.chunk.js"
+  },
+  {
+    "revision": "45e586164808c952663d",
+    "url": "/dict/static/js/5.ed462c13.chunk.js"
+  },
+  {
+    "revision": "aa9047e7d70e08562739",
+    "url": "/dict/static/js/6.220adad7.chunk.js"
+  },
+  {
+    "revision": "2e946aba27f5647e86d2",
+    "url": "/dict/static/js/7.1761a1c2.chunk.js"
+  },
+  {
+    "revision": "22fbb2f437eaed900549",
+    "url": "/dict/static/js/8.8025e454.chunk.js"
+  },
+  {
+    "revision": "f8a1ae280b062fa5489c",
+    "url": "/dict/static/js/9.3358dad6.chunk.js"
+  },
+  {
+    "revision": "08496ef688044a3559df",
+    "url": "/dict/static/js/main.ee1fd3f5.chunk.js"
+  },
+  {
+    "revision": "43989c90b9dd9a642fb9",
+    "url": "/dict/static/js/runtime~main.cce44bf6.js"
+  },
+  {
+    "revision": "cc4daa9d54c97a1badec1f0fd9a327dc",
+    "url": "/dict/static/media/404.cc4daa9d.gif"
+  },
+  {
+    "revision": "2f21dc84f84eb11c60be5697dbdba46d",
+    "url": "/dict/static/media/agenda.2f21dc84.svg"
+  },
+  {
+    "revision": "a54553d4583ed8d97689699d7314edaa",
+    "url": "/dict/static/media/learn.a54553d4.jpg"
+  },
+  {
+    "revision": "f606cbf26c0a18898b96ef6857953a75",
+    "url": "/dict/static/media/loading.f606cbf2.gif"
+  },
+  {
+    "revision": "6bc140c135af169488cc42b94046a3e8",
+    "url": "/dict/static/media/no_word.6bc140c1.png"
+  },
+  {
+    "revision": "ccdb678fb93e6d38d71ad830d28ecadc",
+    "url": "/dict/static/media/notfound.ccdb678f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dict/precache-manifest.a6dcda5e08d6d9abd7d1bf50f200e390.js"
+  "/dict/precache-manifest.13688462d7a4da7114633a6440062872.js"
 );
 
 self.addEventListener('message', (event) => {
